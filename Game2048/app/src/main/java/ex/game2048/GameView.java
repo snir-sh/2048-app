961c6433008b9fc0895fc2362108b795bfdb33f4
shmilove89@gmail.com <shmilove89@gmail.com>
2016-02-17 13:25:45 +0200

    Added a getBoard function

--- Game2048/app/src/main/java/ex/game2048/GameView.java
+++ Game2048/app/src/main/java/ex/game2048/GameView.java
@@ -14,6 +14,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -230,5 +231,7 @@ public class GameView extends View implements GestureDetector.OnGestureListener{
         return true;
     }
 
-
+    public LinkedList<GameRectangle> getBoard() {
+        return board;
+    }
 }
